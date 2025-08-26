cbefd1f386e88eba7ab6edf42357a5e71a801a2a
Mohammad Emran <memran.dhk@gmail.com>
2025-08-26 21:21:55 +0600

    Update main.ts

--- playground/main.ts
+++ playground/main.ts
@@ -10,9 +10,10 @@ app.provide('user', user);
 app.use(MarwaDevtools());
 
 // pages/components are optional; router will hint if neither pages nor routes are present
-const files = (import.meta as any).glob?.('/pages/**/*.marwa') ?? {};
-const hasPages = Object.keys(files).length > 0;
-const routes = hasPages ? undefined : [{ path: '/', component: () => import('./App.marwa') }];
+// const files = (import.meta as any).glob?.('/pages/**/*.marwa') ?? {};
+// const hasPages = Object.keys(files).length > 0;
+// const routes = hasPages ? undefined : [{ path: '/', component: () => import('./App.marwa') }];
 
-app.use(createRouter({ files, routes }));
+//app.use(createRouter({ files, routes }));
+
 app.mount('#app');
